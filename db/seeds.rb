a49514854ef12d0ac1cc50bbb128e443ff5a7e9a
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2011-03-29 13:37:40 -0300

    fixed db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,6 @@
 puts "Empty the MongoDB database"
 Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
 puts "Setting up the default user login"
-user = User.create! :name => 'Douglas', :email => 'dbalexandre@gmail.com', :password => 'fidelio', :password_confirmation => 'fidelio'
+user = User.create! :username => 'dbalexandre', :name => 'Douglas', :email => 'dbalexandre@gmail.com', 
+  :password => 'fidelio', :password_confirmation => 'fidelio'
 puts "New user created: #{user.name}" 

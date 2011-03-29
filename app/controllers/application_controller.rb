e88516393cce03f1fe4129e0cb56fd0f641118b5
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2011-03-29 15:18:13 -0300

    changes in application and lists layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   protect_from_forgery
+
+  def after_sign_in_path_for(resource_or_scope)
+    case resource_or_scope
+    when :user, User
+      user_lists_path current_user
+    else
+      super
+    end
+  end
 end

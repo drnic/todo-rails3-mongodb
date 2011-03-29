c09d7562ec81bc94aefd0c8e5728e3f990fdbe5d
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2011-03-29 16:47:25 -0300

    some changes in layout and fixed some routes

--- app/controllers/users/lists_controller.rb
+++ app/controllers/users/lists_controller.rb
@@ -4,7 +4,40 @@ class Users::ListsController < ApplicationController
 
   def index
     @lists = current_user.lists
-    respond_with(@lists)
+    respond_with(current_user, @lists)
+  end
+
+  def show
+    @list = List.find(params[:id])
+    respond_with(current_user, @list)
+  end
+
+  def new
+    @list = List.new
+    respond_with(current_user, @list)
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    respond_with(current_user, @list)
+  end
+
+  def create
+    @list = current_user.lists.new(params[:list])
+    @list.save
+    respond_with(current_user, @list)
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update_attributes(params[:list])
+    respond_with(current_user, @list)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    respond_with(current_user, @list)
   end
 
 end
